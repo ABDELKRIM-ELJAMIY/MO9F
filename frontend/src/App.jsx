daa6af6113a5477e26ec20d38b7570b709b048c4
ABDELKRIM <eljamiiabdelkarim@gmail.com>
2025-07-15 17:06:50 +0100

    Créer la page NotFound.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@
 import './App.css'
 // import HomePage from './pages/public/HomePage'
 import RegistrationForms from './pages/public/RegistrationForms'
-
+import NotFound from './pages/public/NotFound'
 function App() {
   return (
     <>
@@ -11,6 +11,8 @@ function App() {
 
      
       <RegistrationForms/>
+      <NotFound/>
+      
     </>
   )
 }
